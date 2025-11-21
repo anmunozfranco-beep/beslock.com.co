480fc0a1b5b7f87a1a79a8aa3217b69233ab484d
anmunozfranco-beep <anmunozfranco@gmail.com>
2025-11-21 21:34:29 +0100

    Add files via upload

--- templates/blocks/products-portfolio.php
+++ templates/blocks/products-portfolio.php
@@ -3,37 +3,37 @@ $products = [
   [
     'name'  => 'e-Nova',
     'desc'  => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Seguridad y diseño premium.',
-    'image' => get_stylesheet_directory_uri() . '/assets/images/products/e-nova.webp',
+    'image' => get_stylesheet_directory_uri() . '/assets/images/products/e-nova_.webp',
     'link'  => '#'
   ],
   [
     'name'  => 'e-Orbit',
     'desc'  => 'Soluciones avanzadas para acceder a tu espacio con tecnología y estilo. Lorem ipsum.',
-    'image' => get_stylesheet_directory_uri() . '/assets/images/products/e-orbit.webp',
+    'image' => get_stylesheet_directory_uri() . '/assets/images/products/e-orbit_.webp',
     'link'  => '#'
   ],
   [
     'name'  => 'e-Touch',
     'desc'  => 'Sistema inteligente para puertas modernas. Facilidad, protección y control total.',
-    'image' => get_stylesheet_directory_uri() . '/assets/images/products/e-touch.webp',
+    'image' => get_stylesheet_directory_uri() . '/assets/images/products/e-touch_.webp',
     'link'  => '#'
   ],
   [
     'name'  => 'e-Flex',
     'desc'  => 'Flexibilidad y seguridad en una cerradura inteligente adaptable a cualquier hogar. Lorem ipsum.',
-    'image' => get_stylesheet_directory_uri() . '/assets/images/products/e-flex.webp',
+    'image' => get_stylesheet_directory_uri() . '/assets/images/products/e-flex_.webp',
     'link'  => '#'
   ],
   [
     'name'  => 'e-Shield',
     'desc'  => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Seguridad y diseño premium.',
-    'image' => get_stylesheet_directory_uri() . '/assets/images/products/e-shield.webp',
+    'image' => get_stylesheet_directory_uri() . '/assets/images/products/e-shield_.webp',
     'link'  => '#'
   ],
   [
     'name'  => 'e-Prime',
     'desc'  => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Seguridad y diseño premium.',
-    'image' => get_stylesheet_directory_uri() . '/assets/images/products/e-prime.webp',
+    'image' => get_stylesheet_directory_uri() . '/assets/images/products/e-prime_.webp',
     'link'  => '#'
   ],
 ];
